124080f8782ea9e61945d3138da12f7abc63afd2
MisterDinu <134553073+MisterDinu@users.noreply.github.com>
2023-12-08 03:58:27 -0600

    creación de js y modificación de estilos y otros css, más nuestro servicio

---
+++ js/al.js
@@ -0,0 +1,17 @@
+document.addEventListener('scroll', reveal)
+
+function reveal(){
+    var reveals = document.querySelectorAll('.paso');
+    for(var i = 0; i < reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 200;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
