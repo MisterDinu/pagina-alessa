9058753203a108b37f7a82c2060b870c4eb2e66b
Jaime <jbravorod@outlook.com>
2023-12-20 12:33:54 -0600

    boton volver arriba

--- js/al.js
+++ js/al.js
@@ -1,3 +1,26 @@
+// Función para mostrar el botón cuando se desplaza
+window.onscroll = function() {
+        mostrarBoton();
+    };
+
+function mostrarBoton() {
+    var botonArriba = document.getElementById("btnArriba");
+
+// Se muestra el botón cuando el usuario ha bajado 300px
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        botonArriba.style.display = "block";
+    } else {
+        botonArriba.style.display = "none";
+    }
+}
+
+// Función para ocultar el botón
+function volverArriba() {
+    document.body.scrollTop = 0; // 
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+}
+
+
 // FOR THE NS PASOS SECTION, TEXT BOX APPEARS WHEN SCROLLING
 
 document.addEventListener('scroll', reveal)
