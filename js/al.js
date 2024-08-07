7942b9999710f7802fce554cc3343cb244904b9c
MisterDinu <134553073+MisterDinu@users.noreply.github.com>
2024-08-07 00:41:07 -0600

    Rediseño de testimonios, cambios pequeños en psicólogos, se agregó botón de whats a páginas de psicólogos

--- js/al.js
+++ js/al.js
@@ -6,8 +6,8 @@ window.onscroll = function() {
 function mostrarBoton() {
     var botonArriba = document.getElementById("btnArriba");
 
-// Se muestra el botón cuando el usuario ha bajado 300px
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+// Se muestra el botón cuando el usuario ha bajado 100px
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         botonArriba.style.display = "block";
     } else {
         botonArriba.style.display = "none";
@@ -15,13 +15,14 @@ function mostrarBoton() {
 
     var botonWa = document.getElementById("btnSeguimiento");
 
-// Se muestra el botón cuando el usuario ha bajado 300px
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+// Se muestra el botón cuando el usuario ha bajado 100px
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         botonWa.style.display = "block";
     } else {
         botonWa.style.display = "none";
     }
 }
+
 
 // Función para ocultar el botón
 function volverArriba() {
@@ -29,7 +30,7 @@ function volverArriba() {
     document.documentElement.scrollTop = 0;
 }
 
-// PARA MOSTRAR Y CERRAR CARD
+// PARA MOSTRAR Y CERRAR CARD PASOS
 
 function mostrarModal(idModal) {
     var modal = document.getElementById(idModal);
@@ -66,8 +67,63 @@ function reveal(){
 // PARA ERROR DE TARGET BLANK EN BOTÓN DE WHATSAPP
 
 function agendarSesion() {
-    event.preventDefault();
     var url = "https://wa.me/525636140438?text=¡Hola!%20Me%20gustaría%20agendar%20una%20sesión";
     gtag_report_conversion(url);
     window.open(url, "_blank");
 }
+
+
+// PARA TESTIMONIOS
+
+document.addEventListener("DOMContentLoaded", () => {
+    const carrete = document.querySelector(".carrete");
+
+    if (carrete) {
+        let isDragging = false;
+        let startX;
+        let scrollLeft;
+
+        const startDragging = (e) => {
+            isDragging = true;
+            startX = e.pageX - carrete.offsetLeft;
+            scrollLeft = carrete.scrollLeft;
+        };
+
+        const stopDragging = () => {
+            isDragging = false;
+        };
+
+        const dragging = (e) => {
+            if (!isDragging) return;
+            e.preventDefault();
+            const x = e.pageX - carrete.offsetLeft;
+            const walk = (x - startX) * 2; // La cantidad de desplazamiento
+            carrete.scrollLeft = scrollLeft - walk;
+        };
+
+        carrete.addEventListener("mousedown", startDragging);
+        carrete.addEventListener("mouseleave", stopDragging);
+        carrete.addEventListener("mouseup", stopDragging);
+        carrete.addEventListener("mousemove", dragging);
+    } else {
+        console.error("No se encontró el elemento .carrete");
+    }
+    const scrollLeftBtn = document.getElementById("scroll-left");
+    const scrollRightBtn = document.getElementById("scroll-right");
+
+    // Define la cantidad de desplazamiento en píxeles
+    const scrollAmount = 300;
+
+    // Función para desplazar hacia la izquierda
+    scrollLeftBtn.addEventListener("click", () => {
+        carrete.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    });
+
+    // Función para desplazar hacia la derecha
+    scrollRightBtn.addEventListener("click", () => {
+        carrete.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    });
+
+});
+
+
